31b455d9f58de915091568f09f86ea52df5b641b
Forai <159516259+sailornewborn@users.noreply.github.com>
2026-06-12 02:12:04 +0800

    fixed some resolution bug

--- main.c
+++ main.c
@@ -1,23 +1,36 @@
 #include "stdio.h"
 #ifdef __APPLE__
+#include "CoreGraphics/CoreGraphics.h"
 #include "./build_deps/raylib-6.0_macos/include/raylib.h"
 #import <Cocoa/Cocoa.h>
 #endif
 int main()
 {
-    SetConfigFlags(FLAG_WINDOW_UNDECORATED |
-                   FLAG_WINDOW_TOPMOST |
-                   FLAG_WINDOW_TRANSPARENT);
-    InitWindow(GetMonitorWidth(0), GetMonitorHeight(0),"Window Overlay");
-    #ifdef __APPLE__
-    NSWindow* glfwWindow = (NSWindow*)GetWindowHandle();
-    [glfwWindow setIgnoresMouseEvents:YES]; 
-    #endif
+    int width = 800;
+    int height = 600;
+#ifdef __APPLE__
+    CGDirectDisplayID main_display = CGMainDisplayID();
+    CGDisplayModeRef mode = CGDisplayCopyDisplayMode(main_display);
+    width = (int)CGDisplayModeGetWidth(mode);
+    height = (int)CGDisplayModeGetHeight(mode);
+    SetConfigFlags(
+        FLAG_WINDOW_TOPMOST |
+        FLAG_WINDOW_TRANSPARENT);
+#endif
+
+    InitWindow(width, height, "Window Overlay");
+
+#ifdef __APPLE__
+    NSWindow *glfwWindow = (NSWindow *)GetWindowHandle();
+    [glfwWindow setIgnoresMouseEvents:YES];
+    SetWindowState(FLAG_WINDOW_UNDECORATED);
+    SetWindowPosition(0, 0);
+#endif
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLANK);
-        DrawFPS(10,100);
+        DrawFPS(10, 100);
         EndDrawing();
     }
     CloseWindow();
